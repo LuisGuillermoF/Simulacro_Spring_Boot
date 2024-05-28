3518388dc28c694953c5d047fe60e91daeb5643f
alejandroava <alejandroalzate.ava@gmail.com>
2024-05-28 12:45:20 -0500

    algunas entidaddes sin FK

--- src/main/java/com/riwi/Simulacro_Spring_Boot/domain/entities/enrollments.java
+++ src/main/java/com/riwi/Simulacro_Spring_Boot/domain/entities/enrollments.java
@@ -1,30 +1,23 @@
 package com.riwi.Simulacro_Spring_Boot.domain.entities;
 
-import com.riwi.Simulacro_Spring_Boot.util.role;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class user {
+@Builder
+public class enrollments {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nameUser;
-    private String passwordUser;
-    private String emailUser;
-    private String fullName;
-    private role role;
+    private LocalDateTime date;
 
-    
-    
+    private Long idUser;
 }
